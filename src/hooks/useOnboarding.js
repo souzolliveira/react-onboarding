d61f66764c5e26ac45ee1b4a678fb9e7ec5378f7
Emanuel Oliveira <emanuel.oliveira@pd.griaule>
2022-09-14 18:21:21 -0300

    fix: executing function before highlight element always and export more states to handle better

--- src/hooks/useOnboarding.js
+++ src/hooks/useOnboarding.js
@@ -90,6 +90,7 @@ const OnboardingProvider = ({ children }) => {
     if (isTutorialStarted && steps.length > 0) {
       const element = document.querySelector(`[data-onboarding="${steps[index]?.dataOnboarding}"]`);
       if (element) {
+        steps[index].executeBefore();
         element.scrollIntoView();
         setHighlightedElementWidth(element.offsetWidth);
         setHighlightedElementHeight(element.offsetHeight);
@@ -113,19 +114,12 @@ const OnboardingProvider = ({ children }) => {
   };
 
   const handleNext = async () => {
-    if (index < steps.length - 1) {
-      await steps[index + 1].executeBefore();
-      setIndex(state => state + 1);
-    } else {
-      handleSkip();
-    }
+    if (index < steps.length - 1) setIndex(state => state + 1);
+    else handleSkip();
   };
 
   const handlePrevious = async () => {
-    if (index !== 0) {
-      await steps[index - 1].executeBefore();
-      setIndex(state => state - 1);
-    }
+    if (index !== 0) setIndex(state => state - 1);
   };
 
   const onboarding = () => {
@@ -160,7 +154,10 @@ const OnboardingProvider = ({ children }) => {
         setSteps,
         setTexts,
         setSize,
+        isTutorialStarted,
         setIsTutorialStarted,
+        isTutorialFinished,
+        setIsTutorialFinished,
         handleSkip,
         handleNext,
         handlePrevious,

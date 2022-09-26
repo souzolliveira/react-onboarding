2703d4d30e9210e6e6e36c4410914c161f781fc9
Emanuel Oliveira <emanuel.oliveira@pd.griaule>
2022-09-26 17:30:08 -0300

    fix: removing incorrect eslint comment

--- src/hooks/useOnboarding.js
+++ src/hooks/useOnboarding.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-no-constructed-context-values */
 import PropTypes from 'prop-types';
 import React, { useState, useEffect, useMemo, createContext, useContext } from 'react';
 
